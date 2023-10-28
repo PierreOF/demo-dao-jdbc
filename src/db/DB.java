e94aa1d3b197bf3e98de54b1ae6ae50008d9f0ef
PierreOF <marcos.pierre@upe.br>
2023-10-28 14:28:08 -0300

    feat: FindById Implementation

--- src/db/DB.java
+++ src/db/DB.java
@@ -2,9 +2,7 @@ package db;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Properties;
 
 
@@ -27,15 +25,35 @@ public class DB {
         return conn;
     }
 
-    public static void closeConnection(){
-        if(conn!=null){
-            try{
+    public static void closeConnection() {
+        if (conn != null) {
+            try {
                 conn.close();
-            }catch (SQLException e){
+            } catch (SQLException e) {
                 throw new DbException(e.getMessage());
             }
         }
     }
+    public static void closeStatement(Statement st) {
+        if (st != null) {
+            try {
+                st.close();
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
+    }
+
+    public static void closeResultSet(ResultSet rs) {
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
+    }
+
 
     private static Properties loadlProperties(){
         try (FileInputStream fs = new FileInputStream("db.properties")){
