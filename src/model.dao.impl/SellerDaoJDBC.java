790904f83346542406eaf41d9ac883c8f3bc0902
PierreOF <marcos.pierre@upe.br>
2023-10-28 14:07:46 -0300

    feat: SellerDaoJDBC and DaoFactory

---
+++ src/model.dao.impl/SellerDaoJDBC.java
@@ -0,0 +1,33 @@
+package model.dao.impl;
+
+import model.dao.SellerDao;
+import model.entities.Seller;
+
+import java.util.List;
+
+public class SellerDaoJDBC implements SellerDao {
+    @Override
+    public void insert(Seller obj) {
+
+    }
+
+    @Override
+    public void uptade(Seller obj) {
+
+    }
+
+    @Override
+    public void deleteById(Integer Id) {
+
+    }
+
+    @Override
+    public Seller findById(Integer Id) {
+        return null;
+    }
+
+    @Override
+    public List<Seller> findAll() {
+        return null;
+    }
+}
